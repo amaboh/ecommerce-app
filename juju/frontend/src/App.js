fcc5cd50604488970063b84ccf0d3c974f48167c
amaboh <amabocanon@gmail.com>
2022-05-03 11:47:51 +0100

    setup environment

---
+++ juju/frontend/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Juju</a>
+      </header>
+      <main>
+          list products
+      </main>
+    </div>
+  );
+}
+
+export default App;
