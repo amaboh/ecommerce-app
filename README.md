619efd5fa3fd35793eccdff73688606fcc6f000f
amaboh <amabocanon@gmail.com>
2022-05-03 20:34:10 +0100

    product display

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # ecommerce-app
 Fully functional ecommerce app
+
+#This app is built based on clone version of amazon ecommerce site
+
+#The main stacks use in this project is the MERN stacks. Mongo, Express, React and NodeJs
+
+#Main functionalities include the
+-Product listing
+-Signup and login 
+-Shopping cart 
+-
--- juju/frontend/src/App.js
+++ juju/frontend/src/App.js
@@ -1,3 +1,6 @@
+import data from "./data";
+
+
 function App() {
   return (
     <div>
@@ -5,9 +8,24 @@ function App() {
         <a href="/">Juju</a>
       </header>
       <main>
-          list products
+      <h1>Featured Products</h1>
+        <div className="products">
+              {
+                data.products.map(product => (
+                  <div key={product.id} className="product">
+                      <a href={`/product/${product.id}`}> 
+                        <img src={product.image} alt={product.name} />
+                        </a>
+                         <div className="product-info">
+                            <p>{product.name}</p>
+                            <p>{product.price}</p> 
+                        </div>
+                  </div>
+                ))
+              }
+          </div>
       </main>
-    </div>
+    </div>  
   );
 }
 
